72d36e5a730892b021f9a0fa55930bf846fab1c6
AlrightyTighty <ileikpotatozzzz@gmail.com>
2025-03-21 22:46:40 -0500

    pulled out the promise into the GET handler, we can get exercises now yalllll

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const exercisesAPI = require('./src/workout_api.js')
+const express = require("express");
+const exercisesAPI = require("./src/workout_api.js");
 const app = express();
 
 app.use(express.json());
@@ -7,13 +7,19 @@ app.use(express.json());
 const port = process.env.PORT || 3001;
 
 app.get("/exercises", async (req, res) => {
-  const data = exercisesAPI.getResponse();
-  const allExercises = exercisesAPI.parseResponse(data);
-  const filteredExercises = excersisesAPI.getExercises(allExercises, req.body.level, req.body.equipment, rep.body.primaryMuscles);
+  const dataPromise = exercisesAPI.getResponse();
+  dataPromise
+    .then((response) => {
+      const data = response.data;
+      const allExercises = exercisesAPI.parseResponse(data);
+      const filteredExercises = exercisesAPI.getExercises(allExercises, req.body.level, req.body.equipment, req.body.primaryMuscles);
+      res.send(filteredExercises);
+    })
+    .catch((error) => console.error("Error fetching data:", error));
 });
 
 const server = app.listen(port, () => {
-    console.log(`Server Running!`);
+  console.log(`Server Running!`);
 });
 
 module.exports = server;
--- src/workout_api.js
+++ src/workout_api.js
@@ -4,51 +4,39 @@ const app = express(); //idk bout this
 const axios = require("axios");
 
 class Exercise {
-  constructor (name, level, equipment, primaryMuscles, instructions){
+  constructor(name, level, equipment, primaryMuscles, instructions) {
     this.name = name;
     this.level = level;
     this.equipment = equipment;
     this.primaryMuscles = primaryMuscles;
     this.instructions = instructions;
-  };
+  }
 }
 
 // returns response from API
 function getResponse() {
-  const url =
-    "https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/dist/exercises.json";
-  return axios
-    .get(url)
-    .then((response) => response.data)
-    .catch((error) => console.error("Error fetching data:", error));
+  const url = "https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/dist/exercises.json";
+  return axios.get(url);
 }
 
 // parseResponse expects the response frorm getResponse
 function parseResponse(response) {
-  return response.map((exercise) => new Exercise(
-      exercise.name,
-      exercise.level,
-      exercise.equipment,
-      exercise.primaryMuscles,
-      exercise.instructions
-    ));
+  return response.map((exercise) => new Exercise(exercise.name, exercise.level, exercise.equipment, exercise.primaryMuscles, exercise.instructions));
 }
 
 // return exercises based on array of exercises, level, equipment, and primaryMuscles
 function getExercises(allExercises, level, equipment, primaryMuscles) {
-  const matchingExercises = allExercises.filter(exercise => {
-    return (!level || exercise.level === level) &&
-           (!equipment || exercise.equipment === equipment) &&
-           (!primaryMuscles || primaryMuscles.length ===  0 || exercise.primaryMuscles.some(muscle => primaryMuscles.includes(muscle)));
-  }).map(exercise => {
-    return new Exercise(
-      exercise.name,
-      exercise.level,
-      exercise.equipment,
-      exercise.primaryMuscles,
-      exercise.instructions
-    );
-  });
+  const matchingExercises = allExercises
+    .filter((exercise) => {
+      return (
+        (!level || exercise.level === level) &&
+        (!equipment || exercise.equipment === equipment) &&
+        (!primaryMuscles || primaryMuscles.length === 0 || exercise.primaryMuscles.some((muscle) => primaryMuscles.includes(muscle)))
+      );
+    })
+    .map((exercise) => {
+      return new Exercise(exercise.name, exercise.level, exercise.equipment, exercise.primaryMuscles, exercise.instructions);
+    });
 
   return matchingExercises;
 }
@@ -57,5 +45,5 @@ module.exports = {
   getResponse,
   parseResponse,
   getExercises,
-  Exercise
+  Exercise,
 };
