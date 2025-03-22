604750f65cb0f6b3841af52ed4eabc180df6cec4
Olivia Wright <oliviawright@MacBook-Pro-646.attlocal.net>
2025-03-22 11:05:55 -0500

    more functions for weight and progress

--- src/progress_functions.js
+++ src/progress_functions.js
@@ -1,34 +1,18 @@
-const { User } = require('../mongo/userSchema');
+const { normalizeDateToISO, addToArray, getArrayItem, getField } = require('./common_functions');
 
-function normalizeDateToISO(date) {
-    const dateObj = new Date(date);
-    dateObj.setHours(0, 0, 0, 0);
-    return dateObj.toISOString().split('T')[0]; 
-}
-
-async function addProgress(id, progressData){
-    const user = await User.findById(id);
-    if(!user){
-        throw new Error('User not found');
-    }
-    user.progress.push(progressData);
-    await user.save();
-
-    return user.progress;
+async function addProgress(id, progressData) {
+    return await addToArray(id, 'progress', progressData);
 }
 
 async function getProgress(id, date) {
-    const user = await User.findById(id);
-    if (!user) {
-        throw new Error('User not found');
-    }
-
-    const inputDateString = normalizeDateToISO(date);
-
-    const progressFromDate = user.progress.filter(progress => {
-        const progressDateString = normalizeDateToISO(progress.date);
-        return progressDateString === inputDateString;
-    });
-
-    return progressFromDate;
+    return await getArrayItem(id, 'progress', date);
 }
+
+async function getAllProgress(id) {
+    return await getField(id, 'progress');
+}
+module.exports = {
+    addProgress,
+    getProgress,
+    getAllProgress
+};
