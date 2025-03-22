42d24a5f1f4e9826dcc39fde535d63fd1de8d2e0
Liv <olivianwright11@gmail.com>
2025-03-22 12:30:49 -0500

    Update users.js

--- mongo/routers/users.js
+++ mongo/routers/users.js
@@ -12,7 +12,7 @@ router.post("/", async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send("User already registered.");
 
-    user = new User(_.pick(req.body, ['name', 'username', 'email', 'password', 'weight']));
+    user = new User(_.pick(req.body, ['name', 'username', 'email', 'password', 'weight', 'progress']));
 
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
