1974d546524720e3beab7339ac9bb54011b8985c
vinnydycruz <155778203+vinnydycruz@users.noreply.github.com>
2025-03-22 13:19:35 -0500

    fixing syntax for post router

--- mongo/routers/posts.js
+++ mongo/routers/posts.js
@@ -6,15 +6,15 @@ const bcrypt = require('bcrypt');
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-    req.body.userId = req.user.id;
+    req.body.userId = req.user._id;
 
     const { error } = validatePost(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    let post = await Post.findOne({ userId: req.user.id, description: req.body.description });
+    let post = await Post.findOne({ userId: req.user._id, description: req.body.description });
     if (post) return res.status(400).send("Post already exists");
 
-    post = new Post(_.pick(req.body, ["userName", "image", "description", "createdAt", "isPosted"]));
+    post = new Post(_.pick(req.body, ["userId", "userName", "image", "description", "createdAt", "isPosted"]));
     await post.save();
     res.send(_.pick(post, ["_id", "userName", "description", "createdAt", "isPosted"]));
 });
